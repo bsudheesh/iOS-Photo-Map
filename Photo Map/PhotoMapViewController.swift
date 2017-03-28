e9775859aa840ae936d786ca2b4b856573bfc259
Sudheesh Bhattarai <sudheesh.bhattarai@bison.howard.edu>
2017-03-28 17:26:43 -0400

    Milestone 2 completed.

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class PhotoMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var cameraButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // One degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        // San Francisco Lat, Long = latitude: 37.783333, longitude: -122.416667
+        let mapCenter = CLLocationCoordinate2D(latitude: 37.783333, longitude: -122.416667)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: mapCenter, span: mapSpan)
+        // Set animated property to true to animate the transition to the region
+        mapView.setRegion(region, animated: false)
+        addPin()
 
         // Do any additional setup after loading the view.
     }
@@ -19,6 +34,14 @@ class PhotoMapViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func addPin() {
+        let annotation = MKPointAnnotation()
+        let locationCoordinate = CLLocationCoordinate2D(latitude: 37.779560, longitude: -122.393027)
+        annotation.coordinate = locationCoordinate
+        annotation.title = "Founders Den"
+        mapView.addAnnotation(annotation)
     }
     
 
