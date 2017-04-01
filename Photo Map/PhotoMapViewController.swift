760f8a241cd38f63e509ef5bbbf5418fd006b9e1
Sudheesh Bhattarai <sudheesh.bhattarai@bison.howard.edu>
2017-03-31 20:47:05 -0400

    Milestone 3 completed.

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class PhotoMapViewController: UIViewController {
+class PhotoMapViewController: UIViewController,  UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
     @IBOutlet weak var cameraButton: UIButton!
+    let picker = UIImagePickerController()
+    
+    var pickedImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         // One degree of latitude is approximately 111 kilometers (69 miles) at all times.
         // San Francisco Lat, Long = latitude: 37.783333, longitude: -122.416667
@@ -42,6 +46,41 @@ class PhotoMapViewController: UIViewController {
         annotation.coordinate = locationCoordinate
         annotation.title = "Founders Den"
         mapView.addAnnotation(annotation)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    @IBAction func onCameraButtonClick(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        
+        
+        
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        
+        self.present(vc, animated: true, completion: nil)
+        
     }
     
 
